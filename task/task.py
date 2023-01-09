f61c149ea436602d86089bc5c7965a543b569736
DAF201 <daf201@blink-in.com>
2023-01-09 02:04:08 -0600

    Update task.py

--- task/task.py
+++ task/task.py
@@ -34,41 +34,31 @@ class task:
             # default immediately start unless there is a 3 task at front
             start_with_in = self.kwargs['start_with_in'] if 'start_with_in' in self.kwargs.keys(
             ) else 0
-
             self.priority = priority(
-                task_priority, start_with_in)
+                task_priority, start_with_in=start_with_in)
 
+        # task need to be finished with in x secs?
         self.time_out = self.kwargs['time_out'] if 'time_out' in kwargs_keys else None
-        self.repeat_times = self.kwargs['repeat_times'] if 'repeat_times' in kwargs_keys else None
-        self.repeat_interval = self.kwargs['repeat_interval'] if 'repeat_interval' in kwargs_keys else None
-
-        self.on_create = self.kwargs['on_create'] if 'on_create' in kwargs_keys else None
-        self.start_processing = self.kwargs['start_processing']if 'start_processing' in kwargs_keys else None
-        self.finish_processing = self.kwargs['finish_processing']if 'finish_processing' in kwargs_keys else None
-        self.on_remove = self.kwargs['on_remove']if 'on_remove' in kwargs_keys else None
+        # when task didn't finished with in x secs, do something?
         self.on_timeout = self.kwargs['on_timeout']if 'on_timeout' in kwargs_keys else None
 
+        # task need to be repeated?
+        self.repeat_times = self.kwargs['repeat_times'] if 'repeat_times' in kwargs_keys else None
+        # interval between each repeat?
+        self.repeat_interval = self.kwargs['repeat_interval'] if 'repeat_interval' in kwargs_keys else None
+
+        # hooks
+        self.on_create = self.kwargs['on_create'] if 'on_create' in kwargs_keys else None
+        self.on_start = self.kwargs['on_start']if 'on_start' in kwargs_keys else None
+        self.on_finish = self.kwargs['on_finish']if 'on_finish' in kwargs_keys else None
+        self.on_remove = self.kwargs['on_remove']if 'on_remove' in kwargs_keys else None
+
     def __str__(self) -> str:
-        if (type(self.task_content) in [BuiltinFunctionType, FunctionType]):
-            return """
+        task_body = self.task_content.__name__ if (type(self.task_content) in [
+                                                   BuiltinFunctionType, FunctionType]) else self.task_content.__class__.__name__
+        return """
                 id: %s
-                function: %s 
+                task: %s 
                 params: %s
-                time out: %s
-                interval: %s
                 kwargs: %s
-                """ % (self.id, self.task_content.__name__, str(self.params), str(self.time_out), str(self.interval), str(self.kwargs))
-        else:
-            return """
-                id: %s
-                object: %s 
-                params: %s
-                time out: %s
-                interval: %s
-                kwargs: %s
-                """ % (self.id, self.task_content.__class__.__name__, str(self.params), str(self.time_out), str(self.interval), str(self.kwargs))
-
-
-t = task("test", None, None, 1, on_timeout=print)
-print(t.on_timeout)
-print(t)
+                """ % (self.id, task_body, str(self.params), str(self.kwargs))
