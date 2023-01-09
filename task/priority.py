8da2dce9610ebbd0591f62a51de53b6c21e2f3aa
DAF201 <daf201@blink-in.com>
2023-01-09 12:03:42 -0600

    update

--- task/priority.py
+++ task/priority.py
@@ -1,5 +1,5 @@
 class priority:
-    def __init__(self, task_priority, **kwargs) -> None:
+    def __init__(self, task_priority: int = 0, **kwargs) -> None:
         self.task_priority = task_priority
         if kwargs != {}:
             self.start_with_in = kwargs['start_with_in']
@@ -7,5 +7,5 @@ class priority:
         else:
             self.start_with_in = 0
 
-    def __call__(self):
+    def __call__(self) -> int:
         return self.task_priority
--- task/task.py
+++ task/task.py
@@ -4,6 +4,8 @@ from types import FunctionType, BuiltinFunctionType, NoneType
 from priority import priority
 from error_and_exception import TYPE_ERROR, raise_exception
 from inspect import currentframe, isclass
+from typing import Any
+from gc import collect
 
 
 class task:
@@ -33,12 +35,13 @@ class task:
         #  1: somehow important task, can start earlier than 0 task
         #  2: important, require a start_with_in range
         #  3: need to be started immediately, put at front of all other tasks
-        if task_priority == 2:
+        if task_priority >= 2:
             # default immediately start unless there is a 3 task at front
             start_with_in = self.kwargs['start_with_in'] if 'start_with_in' in self.kwargs.keys(
             ) else 0
-            self.priority = priority(
-                task_priority, start_with_in=start_with_in)
+
+        self.priority = priority(
+            task_priority, start_with_in=start_with_in) if task_priority > 1 else priority(task_priority)
 
         # task need to be finished with in x secs?
         self.time_out = self.kwargs['time_out'] if 'time_out' in kwargs_keys else None
@@ -53,8 +56,14 @@ class task:
         # hooks
         self.on_create = self.kwargs['on_create'] if 'on_create' in kwargs_keys else None
         self.on_start = self.kwargs['on_start']if 'on_start' in kwargs_keys else None
+        self.on_pause = self.kwargs['on_pause']if 'on_pause' in kwargs_keys else None
+        self.on_resume = self.kwargs['on_resume']if 'on_resume' in kwargs_keys else None
         self.on_finish = self.kwargs['on_finish']if 'on_finish' in kwargs_keys else None
         self.on_remove = self.kwargs['on_remove']if 'on_remove' in kwargs_keys else None
+
+        # Because this is init so on_create cannot return anything
+        if self.on_create != None:
+            self.on_create()
 
     def type_checking(self, task_id, task_content, task_params, task_priority):
         # id is str or int
@@ -73,6 +82,20 @@ class task:
             raise_exception(TYPE_ERROR, currentframe(),
                             kwargs={"variable": "task.task_priority"})
 
+    def __eq__(self, __o: object) -> bool:
+        try:
+            return (self.task_id == __o.task_id)
+        except:
+            return False
+
+    def __enter__(self) -> Any:
+        if self.on_start != None:
+            return self.on_start()
+
+    def __exit__(self, type: Any, value: Any, trace: Any) -> Any:
+        if self.on_finish != None:
+            return self.on_finish()
+
     def __str__(self) -> str:
         return """
                 id: %s
@@ -80,6 +103,12 @@ class task:
                 params: %s
                 kwargs: %s
                 """ % (self.task_id, self.task_content, str(self.task_params), str(self.kwargs))
+
+    def __del__(self):
+        # on remove cannot be returned
+        if self.on_remove != None:
+            self.on_remove()
+        collect()
 
 
 # task(1, None)
