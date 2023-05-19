cdd78f659f760ffb3940c7b35a2929a9dfaa670e
daf201 <daf201@blink-in.com>
2023-05-18 19:54:04 -0500

    Update test.py

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ a = Thread(target=test)
 b = Thread(target=test)
 setattr(a, "onfinish", t)
 setattr(a, "onfinishargs", "123")
-setattr(a, "onfinishtimeout", 5)
+setattr(a, "onfinishtimeout", 5)# this arrt is discard
 a.start()
 b.start()
 print(task_timer.TASK_TIMER_WAITLIST)
