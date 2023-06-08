b0f368b48dcdb941a64d9b816ac3145f52d9e91c
daf201 <daf201@blink-in.com>
2023-06-08 13:48:30 -0500

    Update task_body.py

--- task_body.py
+++ task_body.py
@@ -1,41 +1,52 @@
-from task_tools import *
+from task_tools import Thread, Event, settrace, Any
+
+# this class is good now, add feature to other classes
 
 
 class task(Thread):
+    num_of_thread_created = 0
     num_of_thread_running = 0
 
-    def __init__(self, priority=0, end_callback=None, return_able=False, group=None, target=None, name=None,
+    def __init__(self, priority=0,  return_able=False, time_out=None, group=None, target=None, name=None,
                  args=(), kwargs={}, daemon=False) -> None:
         """priority from -2 to 2, end callback will be called after finished"""
+        self.num_of_thread_created += 1
+
         Thread.__init__(self, group=group, target=target, name=name,
                         args=args, kwargs=kwargs, daemon=daemon)
-        self._return = None
 
         # for blocking
         self.__pause_event = Event()
 
         # pause and terminate flag
-        self.__pause_flag = False
+        self.__pause_flag = True
         self.__terminate_flag = False
 
         # task id for trace
         self.task_id = hash(self.getName()+str(args)+str(kwargs))
         self.priority = priority
-        self.end_callback = end_callback
         self.return_able = return_able
+        self.time_out = time_out
+        if return_able:
+            self._return = None
 
         Thread.run = self.__result_run
 
     def start(self) -> None:
+        self.__pause_flag = False
         self.__run_backup = self.run
         self.run = self.__run
+        self.num_of_thread_running += 1
         Thread.start(self)
 
     def __result_run(self):
         """over ride the Thread.run"""
         try:
             if self._target is not None:
-                self._return = self._target(*self._args, **self._kwargs)
+                if self.return_able:
+                    self._return = self._target(*self._args, **self._kwargs)
+                else:
+                    self._target(*self._args, **self._kwargs)
         finally:
             del self._target, self._args, self._kwargs
 
@@ -69,14 +80,20 @@ class task(Thread):
         return self.__localtrace
 
     def pause(self) -> None:
-        self.__pause_flag = True
+        if self.is_running:
+            self.num_of_thread_running -= 1
+            self.__pause_flag = True
 
     def resume(self) -> None:
-        self.__pause_flag = False
-        self.__pause_event.set()
+        if self.__pause_flag and self.is_alive() and (not self.__terminate_flag):
+            self.num_of_thread_running += 1
+            self.__pause_flag = False
+            self.__pause_event.set()
 
     def terminate(self) -> None:
-        self.__terminate_flag = True
+        if not self.__terminate_flag:
+            self.num_of_thread_running -= 1
+            self.__terminate_flag = True
 
     def is_terminated(self) -> bool:
         return self.__terminate_flag
@@ -87,6 +104,15 @@ class task(Thread):
     def is_running(self) -> bool:
         return self.is_alive() and (not self.__pause_flag) and (not self.__terminate_flag)
 
+    def is_ended(self) -> bool:
+        return not self.is_alive() and (not self.__pause_flag) and (not self.__terminate_flag)
+
     def __get_result__(self) -> Any:
         """cannot guarentee finished, will add a flag later but my lunch time is about to end"""
-        return self._return
+        if self.return_able:
+            self.join()
+            return self._return
+        return None
+
+    def __del__(self):
+        self.num_of_thread_created -= 1
