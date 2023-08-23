c39646df5f5760d61bf8fbce41df3b7d32bfc781
fzy-0429 <130481849+fzy-0429@users.noreply.github.com>
2023-08-22 23:57:02 -0500

    Update pthread_minus_minus.hpp

--- pthread_minus_minus.hpp
+++ pthread_minus_minus.hpp
@@ -2,68 +2,80 @@
 #define PTHREAD_MINUS
 #include <unistd.h>
 #include <pthread.h>
-#include <stdio.h>
 #include <signal.h>
-#define lock pthread_mutex_t
-#define cond pthread_cond_t
-#define t_wait(conditional)                              \
-    {                                                    \
-        if (condition.p != 0)                            \
-            pthread_cond_wait(condition.c, condition.l); \
+#define t_wait(condition)                                      \
+    {                                                          \
+        if (condition.p != 0)                                  \
+            pthread_cond_wait(&(condition.c), &(condition.l)); \
     }
-struct __cond_var
+#define t_term(condition)     \
+    {                         \
+        if (condition.t != 0) \
+            return nullptr;   \
+    }
+typedef struct __cond_var
 {
-    lock *l;
-    cond *c;
+    pthread_mutex_t l;
+    pthread_cond_t c;
     int p;
-};
-typedef __cond_var cond_var;
-typedef void *(*func)(cond_var &, void *);
+    int t;
+} cond_var;
 typedef void *param;
-
+typedef void *t_ret;
+typedef t_ret (*func)(cond_var &, param);
 class task
 {
 public:
     task(func subroutine, param args)
     {
-
         this->subroutine = subroutine;
-        this->args[0] = args;
-        this->private_condition.l = &(this->private_lock);
-        this->private_condition.c = &(this->private_cond);
+        this->args = args;
+        this->private_condition.l = PTHREAD_MUTEX_INITIALIZER;
+        this->private_condition.c = PTHREAD_COND_INITIALIZER;
         this->private_condition.p = 0;
         pthread_create(
             &(this->tid), NULL, [](void *self) -> void *
             {static_cast<task *>(self)->__exec();return nullptr; },
             this);
     }
-    int __start()
+    void __start()
     {
-        return pthread_cond_signal(&(this->private_cond));
+        sleep(1);
+        this->private_condition.p = 0;
+        pthread_cond_signal(&(this->private_condition.c));
     }
-    int __exec()
+    void __exec()
     {
-        // pause the function, wait for the start()
-        pthread_cond_wait(&(this->private_cond), &(this->private_lock));
-        this->subroutine(this->private_condition, this->args);
-        return 0;
+        pthread_cond_wait(&(this->private_condition.c), &(this->private_condition.l));
+        this->result = this->subroutine(this->private_condition, this->args);
     }
     void __resume()
     {
         this->private_condition.p = 0;
-        pthread_cond_signal(&(this->private_cond));
+        pthread_cond_signal(&(this->private_condition.c));
     }
     void __pause()
     {
         this->private_condition.p = 1;
     }
+    void __term()
+    {
+        this->private_condition.t = 1;
+    }
+    t_ret __get_res()
+    {
+        return this->result;
+    }
+    ~task()
+    {
+        pthread_join(this->tid, nullptr);
+    }
 
 private:
     func subroutine;
-    param args[2];
+    param args;
+    t_ret result;
     pthread_t tid;
-    pthread_mutex_t private_lock = PTHREAD_MUTEX_INITIALIZER;
-    pthread_cond_t private_cond = PTHREAD_COND_INITIALIZER;
     cond_var private_condition;
 };
 #endif
